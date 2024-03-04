39177b496980293b58741cb497c7aec82cebf0bd
vishalrana888 <vishalrana888@gmail.com>
2024-03-05 00:43:53 +0530

    Retrieving data from databases

--- models/product.js
+++ models/product.js
@@ -13,21 +13,18 @@ module.exports = class Product {
 
   save() {
     return db.execute(
-    'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-    [this.title, this.price, this.imageUrl, this.description]
-     );
-   
+      'INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+      [this.title, this.price, this.imageUrl, this.description]
+    );
   }
 
-  static deleteById(id) {
-    
-  }
+  static deleteById(id) {}
 
   static fetchAll() {
     return db.execute('SELECT * FROM products');
   }
 
   static findById(id) {
-    
+    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
   }
 };
