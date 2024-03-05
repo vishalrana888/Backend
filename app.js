52582a7eb6a1e10f721e52fb9d3737b124fa939d
vishalrana888 <vishalrana888@gmail.com>
2024-03-06 03:07:59 +0530

    use the sequelize to do all the functionality with the help of SQL database.

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const path = require('path');
 var cors = require('cors')
-const contactusRouter = require('./routes/contactus');
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const contactusRouter = require('./routes/contactus');
 const errorController = require('./controllers/error');
-const db = require('./util/database');
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
-app.use('/contactus', contactusRouter);
+
 
 app.use(cors())
 app.set('view engine', 'ejs');
@@ -16,14 +17,26 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+app.use('/contactus', contactusRouter);
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-
 app.use(errorController.get404);
 
-app.listen(3000);
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+
+sequelize
+  .sync({ force: true })
+  .then(result => {
+    // console.log(result);
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
